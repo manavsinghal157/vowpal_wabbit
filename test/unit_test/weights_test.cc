c120022b186add1e70374b22f2fbee4d71624d63
manavsinghal157 <manav157.os@gmail.com>
2021-11-01 13:03:22 +0530

    Merge conflicts

--- test/unit_test/weights_test.cc
+++ test/unit_test/weights_test.cc
@@ -1,6 +1,12 @@
+<<<<<<< HEAD
+#ifndef STATIC_LINK_VW
+#  define BOOST_TEST_DYN_LINK
+#endif
+=======
 // Copyright (c) by respective owners including Yahoo!, Microsoft, and
 // individual contributors. All rights reserved. Released under a BSD (revised)
 // license as described in the file LICENSE.
+>>>>>>> upstream/master
 
 #include <boost/test/unit_test.hpp>
 #include <boost/mpl/vector.hpp>
@@ -20,9 +26,39 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(test_default_function_weight_initialization_stride
   T w(LENGTH, STRIDE_SHIFT);
   auto weight_initializer = [](weight* weights, uint64_t index) { weights[0] = 1.f * index; };
   w.set_default(weight_initializer);
-  for(size_t i = 0; i < LENGTH; i++)
-  {
-    BOOST_CHECK_CLOSE(w.strided_index(i), 1.f * (i * w.stride()) , FLOAT_TOL);
-  }
+  for (size_t i = 0; i < LENGTH; i++) { BOOST_CHECK_CLOSE(w.strided_index(i), 1.f * (i * w.stride()), FLOAT_TOL); }
 }
 
+BOOST_AUTO_TEST_CASE_TEMPLATE(test_feature_is_activated, T, weight_types)
+{
+  // unit test to check if number of bits equal to the threshold in
+  // bitset set to 1 for a feature returns true
+  int threshold = 10;
+  T w(LENGTH, STRIDE_SHIFT);
+  w.privacy_activation_threshold(threshold);
+  int feature_index = 0;
+  // function to set the bits in bitset to 1 equal to the threshold
+  for (int tag_hash = 0; tag_hash < threshold; tag_hash++)
+  {
+    w.set_tag(tag_hash);
+    w.set_privacy_preserving_bit(feature_index);
+  }
+  BOOST_CHECK_EQUAL(w.is_activated(feature_index), true);
+}
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(test_feature_not_activated, T, weight_types)
+{
+  // unit test to check if number of bits less than the threshold in
+  // bitset set to 1 for a feature returns false
+  int threshold = 10;
+  T w(LENGTH, STRIDE_SHIFT);
+  w.privacy_activation_threshold(threshold);
+  int feature_index = 0;
+  // function to set the bits in bitset to 1 equal to the (threshold-1)
+  for (int tag_hash = 0; tag_hash < (threshold - 1); tag_hash++)
+  {
+    w.set_tag(tag_hash);
+    w.set_privacy_preserving_bit(feature_index);
+  }
+  BOOST_CHECK_EQUAL(w.is_activated(feature_index), false);
+}
